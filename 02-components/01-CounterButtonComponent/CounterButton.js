66bfc5d8c12636c86d1c35134b456382a3d67d5d
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-05-01 16:47:26 +0300

    Solve 02.01(copypast) and 02.02

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,15 +3,24 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  props: ['count'],
-  // props: {
-  //   count: {
-  //     type: [Number, String],
-  //     required: true,
-  //   },
+  // Теперь текущее значение счётчика приходит от родителя через входной параметр
+  props: {
+    count: {
+      // Не забываем указать тип
+      type: Number,
+      // Значение по умолчанию - 0
+      default: 0,
+    },
+  },
 
-  //   default: () => 0,
-  // },  
+  emits: ['update:count'],
 
-  template: `<button type="button" v-on:click="$emit('add-count')">{{ count }}</button>`,
+  methods: {
+    increment() {
+      // При клике на кнопку порождаем событие и отправляем новое значение
+      this.$emit('update:count', this.count + 1);
+    },
+  },
+
+  template: `<button type="button" @click="increment">{{ count }}</button>`,
 });
